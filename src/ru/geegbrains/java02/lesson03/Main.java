e1385fe3314f6c6f75d29e488116ac3e5bb2394a
Nikolai-Shcherbina <nikolai.v.shcherbina@gmail.com>
2020-09-04 14:22:32 +0300

    lesson_03 - task 2

--- src/ru/geegbrains/java02/lesson03/Main.java
+++ src/ru/geegbrains/java02/lesson03/Main.java
@@ -6,9 +6,21 @@ import java.util.*;
  * 1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
  * Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
  * Посчитать сколько раз встречается каждое слово.
+ *
+ * 2. Написать простой класс ТелефонныйСправочник, который хранит в себе список фамилий и телефонных номеров.
+ * В этот телефонный справочник с помощью метода add() можно добавлять записи.
+ *
+ * С помощью метода get() искать номер телефона по фамилии.
+ * Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев),
+ * тогда при запросе такой фамилии должны выводиться все телефоны.
+ *
+ * Желательно как можно меньше добавлять своего, чего нет в задании (т.е. не надо в телефонную запись добавлять
+ * еще дополнительные поля (имя, отчество, адрес),
+ * делать взаимодействие с пользователем через консоль и т.д.).
+ * Консоль желательно не использовать (в том числе Scanner),
+ * тестировать просто из метода main() прописывая add() и get().
  */
 public class Main {
-
 
     public static void main(String[] args) {
         List<String> words = Arrays.asList(
@@ -24,5 +36,17 @@ public class Main {
         for (String key : uniqueList) {
             System.out.print(key + ": " + Collections.frequency(words, key) + "; ");
         }
+
+
+        Phonebook phonebook = new Phonebook();
+        phonebook.addContact("Ivanov", "89267777711");
+        phonebook.addContact("Petrov", "89263333333");
+        phonebook.addContact("Sidorov", "89265555555");
+        phonebook.addContact("Ivanov", "89267777722");
+
+        System.out.println(phonebook.getContact("Ivanov"));
+        System.out.println(phonebook.getContact("Petrov"));
+        System.out.println(phonebook.getContact("Sidorov"));
+
     }
 }
